d639dc5217543034a4cfa869726a9f21efff5d32
Tony Baines <tony.baines.1@gmail.com>
2016-08-04 16:01:26 +0100

    More error handlign strategies

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,7 +2,7 @@ pub fn is_unreachable() {
     unreachable!()
 }
 
-pub fn might_fail_simple() -> Option<String> {
+pub fn will_fail_simple() -> Option<String> {
     None
 }
 
@@ -11,7 +11,7 @@ pub struct MyError {
     msg: String
 }
 
-pub fn might_fail_complex() -> Result<String, MyError> {
+pub fn will_fail_complex() -> Result<String, MyError> {
     return Err(MyError {
         msg: "Oh dear!".to_string()
     })
@@ -29,7 +29,7 @@ mod test {
 
     #[test]
     fn simple() {
-        assert_eq!("Nothing", match might_fail_simple() {
+        assert_eq!("Nothing", match will_fail_simple() {
             Some(_) => "Something",
             None => "Nothing",
         })
@@ -37,9 +37,45 @@ mod test {
 
     #[test]
     fn complex() {
-        assert_eq!("Error: MyError { msg: \"Oh dear!\" }".to_string(), match might_fail_complex() {
+        assert_eq!("Error: MyError { msg: \"Oh dear!\" }".to_string(), match will_fail_complex() {
             Ok(_) => "Success".to_string(),
             Err(e) => (format!("Error: {:?}", e)),
+        })
+    }
+
+    #[test]
+    #[should_panic]
+    fn complex_unwrap() {
+        let _x = will_fail_complex().unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Expected")]
+    fn complex_ok_expect() {
+        let _x = will_fail_complex()
+            .ok()
+            .expect("Expected");
+    }
+
+    // Always succeed
+    fn will_succeed_complex() -> Result<String, MyError> {
+        Ok("Success!".to_string())
+    }
+
+    fn wrap_with_try() -> Result<String, MyError> {
+        // Any failure will result in an error
+        try!(will_succeed_complex());
+        try!(will_fail_complex());
+        try!(will_succeed_complex());
+
+        Ok("Eh?".to_string())
+    }
+
+    #[test]
+    fn complex_try() {
+        assert_eq!("Failure", match wrap_with_try() {
+            Ok(_) => "Success",
+            Err(_) => "Failure",
         })
     }
 }
