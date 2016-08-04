c8d45855a4e67d45181caa62dd632b44b6c753e2
Tony Baines <tony.baines.1@gmail.com>
2016-08-04 15:38:42 +0100

    error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,47 @@
+pub fn is_unreachable() {
+    unreachable!()
+}
+
+pub fn might_fail_simple() -> Option<String> {
+    None
+}
+
+pub struct MyError {
+    msg: String
+}
+
+pub fn might_fail_complex() -> Result<String, MyError> {
+    return Err(MyError {
+        msg: "Oh dear!".to_string()
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn panic() {
+        is_unreachable()
+    }
+
+    #[test]
+    fn simple() {
+        assert_eq!("Nothing", match might_fail_simple() {
+            Some(_) => "Something",
+            None => "Nothing",
+        })
+    }
+
+    #[test]
+    fn complex() {
+        assert_eq!("Error: Oh dear!".to_string(), match might_fail_complex() {
+            Ok(_) => "Success".to_string(),
+            Err(e) => (format!("Error: {}", e.msg)),
+        })
+    }
+}
+
+
+
